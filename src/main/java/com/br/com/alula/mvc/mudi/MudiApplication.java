848e6b04fd79cbb13379191b5fc57f4f0bbb3652
tiagoaraujobarreto <tiagoaraujo.barreto@gmail.com>
2021-09-17 20:38:29 -0300

    Primeiro Controller

---
+++ src/main/java/com/br/com/alula/mvc/mudi/MudiApplication.java
@@ -0,0 +1,13 @@
+package com.br.com.alula.mvc.mudi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/com/alula/mvc/mudi/MudiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.com.alula.mvc.mudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
