e0304ab0d04750898e04121ac94f668652001a2d
tiagoaraujobarreto <tiagoaraujo.barreto@gmail.com>
2021-09-23 21:23:30 -0300

    Usando Spring Data JPA

--- src/main/java/com/br/com/alula/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/com/br/com/alula/mvc/mudi/repository/PedidoRepository.java
@@ -1,24 +1,11 @@
 package com.br.com.alula.mvc.mudi.repository;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.br.com.alula.mvc.mudi.model.Pedido;
 
 @Repository
-public class PedidoRepository {
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
-	@PersistenceContext
-	private EntityManager entityManager;
-
-	public List<Pedido> recuperaTodosOsPedidos() {
-		Query query = entityManager.createQuery("SELECT p FROM Pedido p", Pedido.class);
-		return query.getResultList();
-
-	}
 }
