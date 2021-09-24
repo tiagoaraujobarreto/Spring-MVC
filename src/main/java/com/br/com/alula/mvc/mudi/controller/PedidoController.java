8899ba52af470ede3f762bc935c35b48c959e6f9
tiagoaraujobarreto <tiagoaraujo.barreto@gmail.com>
2021-09-24 00:03:01 -0300

    Salvando Pedido

--- src/main/java/com/br/com/alula/mvc/mudi/controller/PedidoController.java
+++ src/main/java/com/br/com/alula/mvc/mudi/controller/PedidoController.java
@@ -1,17 +1,34 @@
 package com.br.com.alula.mvc.mudi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.br.com.alula.mvc.mudi.dto.RequisicaoNovoPedido;
+import com.br.com.alula.mvc.mudi.model.Pedido;
+import com.br.com.alula.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 @RequestMapping("pedido")
 public class PedidoController {
-	
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
 	@GetMapping("formulario")
 	public String formulario() {
 		return "pedido/formulario";
 	}
-	
+
+	@PostMapping("novo")
+	public String novo(RequisicaoNovoPedido requisicao) {
+
+		Pedido pedido = requisicao.toPedido();
+		pedidoRepository.save(pedido);
+
+		return "pedido/formulario";
+	}
 
 }
---
+++ src/main/java/com/br/com/alula/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -0,0 +1,45 @@
+package com.br.com.alula.mvc.mudi.dto;
+
+import com.br.com.alula.mvc.mudi.model.Pedido;
+
+public class RequisicaoNovoPedido {
+	
+	private String nomeProduto;
+	private String urlProduto;
+	private String urlImagem;
+	private String descricao;
+	
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public String getUrlProduto() {
+		return urlProduto;
+	}
+	public void setUrlProduto(String urlProduto) {
+		this.urlProduto = urlProduto;
+	}
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Pedido toPedido() {
+		Pedido pedido = new Pedido();
+		pedido.setDescricao(descricao);
+		pedido.setNomeProduto(nomeProduto);
+		pedido.setUrlDaImagem(urlImagem);
+		pedido.setUrlDoProduto(nomeProduto);
+		return pedido;
+	}
+
+}
