b361f0cf941aa3851392bbb63caef2c82188fba1
tiagoaraujobarreto <tiagoaraujo.barreto@gmail.com>
2021-09-23 23:40:17 -0300

    Menu de navegação + Formulário do pedido

---
+++ src/main/java/com/br/com/alula/mvc/mudi/controller/PedidoController.java
@@ -0,0 +1,17 @@
+package com.br.com.alula.mvc.mudi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("pedido")
+public class PedidoController {
+	
+	@GetMapping("formulario")
+	public String formulario() {
+		return "pedido/formulario";
+	}
+	
+
+}
