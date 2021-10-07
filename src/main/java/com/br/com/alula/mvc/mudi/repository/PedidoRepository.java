8be7d1c67c15041b940fdeaab2112d4f625b2664
tiagoaraujobarreto <tiagoaraujo.barreto@gmail.com>
2021-10-07 20:28:51 -0300

    Rotas do Status

--- src/main/java/com/br/com/alula/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/com/br/com/alula/mvc/mudi/repository/PedidoRepository.java
@@ -1,11 +1,16 @@
 package com.br.com.alula.mvc.mudi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.br.com.alula.mvc.mudi.model.Pedido;
+import com.br.com.alula.mvc.mudi.model.StatusDoPedidio;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+	List<Pedido> findByStatus(StatusDoPedidio aguardando);
 
 }
