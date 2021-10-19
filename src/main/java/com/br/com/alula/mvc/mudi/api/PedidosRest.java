73a4ac9e7a9a5a54d0dfe5d2def3eddfd931afab
tiagoaraujobarreto <tiagoaraujo.barreto@gmail.com>
2021-10-19 20:47:39 -0300

    Criando o RestController

---
+++ src/main/java/com/br/com/alula/mvc/mudi/api/PedidosRest.java
@@ -0,0 +1,31 @@
+package com.br.com.alula.mvc.mudi.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.com.alula.mvc.mudi.model.Pedido;
+import com.br.com.alula.mvc.mudi.model.StatusDoPedidio;
+import com.br.com.alula.mvc.mudi.repository.PedidoRepository;
+
+@RestController
+@RequestMapping("/api/pedidos")
+public class PedidosRest {
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	@GetMapping("aguardando")
+	public List<Pedido> getPedidoAguardandoOfertas(){
+		Sort sort = Sort.by("id").descending();
+		PageRequest paginacao = PageRequest.of(0, 10, sort);
+		
+		return pedidoRepository.findByStatus(StatusDoPedidio.AGUARDANDO, paginacao);
+	}
+
+}
