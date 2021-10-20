e7d3e58ebd3223f0d8562ee4564075f032824066
tiagoaraujobarreto <tiagoaraujo.barreto@gmail.com>
2021-10-19 22:56:48 -0300

    Usando Vue.js

--- src/main/java/com/br/com/alula/mvc/mudi/controller/OfertaController.java
+++ src/main/java/com/br/com/alula/mvc/mudi/controller/OfertaController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/ofertas")
+@RequestMapping("/oferta")
 public class OfertaController {
 
 	@GetMapping
